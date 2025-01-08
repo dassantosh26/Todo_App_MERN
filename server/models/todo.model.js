d7cdf1ac069d8d509a901cbe9691ffa324846770
Santosh Kumar Das <santoshdas.mits@gmail.com>
2025-01-08 14:26:33 +0530

    Created Model schema

---
+++ server/models/todo.model.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Todo = mongoose.model("Todo", todoSchema);
---
+++ server/models/user.model.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
